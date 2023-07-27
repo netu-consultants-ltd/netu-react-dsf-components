ee4a078af95f6bbd322d01ffd998131b68f7ed5c
Porfyrios Vasileiou <porfyriosv@netu.com.cy>
2023-07-27 14:31:18 +0300

    Updates

---
+++ sb-addons/storybook-10/manager-bundle.js
@@ -0,0 +1,4 @@
+try{
+var i=__STORYBOOKADDONS__,{addons:o,types:h,mockChannel:p}=__STORYBOOKADDONS__;var g=__STORYBOOKTHEMING__,{CacheProvider:b,ClassNames:S,Global:x,ThemeProvider:y,background:C,color:N,convert:R,create:s,createCache:k,createGlobal:B,createReset:D,css:G,darken:K,ensure:Y,ignoreSsrWarning:v,isPropValid:P,jsx:A,keyframes:E,lighten:H,styled:I,themes:M,typography:j,useTheme:w,withTheme:U}=__STORYBOOKTHEMING__;var a=s({base:"light",brandTitle:"netu-react-dsf-components",brandUrl:"https://github.com/netu-consultants-ltd/netu-react-dsf-components"});o.setConfig({theme:a});
+}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
+//# sourceMappingURL=manager-bundle.js.map
